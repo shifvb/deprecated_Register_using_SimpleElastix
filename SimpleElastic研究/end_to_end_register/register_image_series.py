c1ee6f3a1257993f607d106bc141904625ee74b4
shifvb <shifvb@gmail.com>
2018-03-08 10:47:49 +0800

    bug fix

--- SimpleElastic研究/end_to_end_register/register_image_series.py
+++ SimpleElastic研究/end_to_end_register/register_image_series.py
@@ -1,9 +1,9 @@
 import os
+import time
 import numpy as np
 import dicom
 from PIL import Image
 import SimpleITK as sitk
-from SimpleElastic研究.end_to_end_register.register_single_image_pair import register_single_image_pair
 from 查看PTCT及标签数据._utils.ImageProcessor import ImageProcessor
 
 I = ImageProcessor()
@@ -13,36 +13,50 @@ __author__ = "shifvb"
 __email__ = "shifvb@gmail.com"
 
 
-def register_image_series_pet_to_ct():
-    # 1. 加载文件
-    ct_series_dir = r"F:\做好的分割数据\迟学梅\CT"
-    pt_series_dir = r"F:\做好的分割数据\迟学梅\PT"
-    ct_series_names = [os.path.join(ct_series_dir, _) for _ in os.listdir(ct_series_dir)]
-    pt_series_names = [os.path.join(pt_series_dir, _) for _ in os.listdir(pt_series_dir)]
-    register_img_index = 100
-    register_ct_img_arr = dicom.read_file(ct_series_names[register_img_index]).pixel_array
-    register_pt_img_arr = dicom.read_file(pt_series_names[register_img_index]).pixel_array
-    # Image.fromarray(I.norm_image(register_ct_img_arr)).show()
-    # Image.fromarray(I.norm_image(register_pt_img_arr)).show()
-    # 2.开始配准
-    fixed_image = sitk.GetImageFromArray(register_ct_img_arr)
-    moving_image = sitk.GetImageFromArray(register_pt_img_arr)
+def register_image_series_pt2ct(ct_series_dir: str, pt_series_dir: str):
+    """
+    将pet图像序列配准到ct图像序列上
+    :param ct_series_dir: ct图像序列文件夹(绝对路径)
+    :param pt_series_dir: pt图像序列文件夹(绝对路径)
+    :return: 配准后的pt图像序列数组
+        class ：np.ndarray
+        dtype ：np.float32
+        shape ：(图像序列数, 配准后图像高度, 配准后图像宽度)
+            若将200张分辨率为128x128的pet图像配准到200张分辨率为512x512的ct图像上，
+            则返回数组的shape为(200, 512, 512)
+    """
+    # 1. 加载图像序列
+    _original_dir = os.path.abspath(os.curdir)
+    os.chdir(ct_series_dir)
+    ct_series = sitk.ReadImage(os.listdir(ct_series_dir))
+    os.chdir(pt_series_dir)
+    pt_series = sitk.ReadImage(os.listdir(pt_series_dir))
+    os.chdir(_original_dir)
+
+    # 2. 配准
+
+    # 2.1 设置配准参数
     parameter_map = sitk.GetDefaultParameterMap("translation")
     parameter_map['RequiredRatioOfValidSamples'] = ['0.05']
-    # 2.1 设置filter
+    # 2.2 初始化filter并设置参数
     elastix_image_filter = sitk.ElastixImageFilter()
-    elastix_image_filter.SetFixedImage(fixed_image)
-    elastix_image_filter.SetMovingImage(moving_image)
+    elastix_image_filter.SetFixedImage(ct_series)
+    elastix_image_filter.SetMovingImage(pt_series)
     elastix_image_filter.SetParameterMap(parameter_map)
-    # 2.2 进行配准计算，得出配准结果
+    # 2.3 进行配准计算
     elastix_image_filter.Execute()
+    # 2.4 得出配准结果
     result_image = elastix_image_filter.GetResultImage()
-    Image.fromarray(255 - sitk.GetArrayFromImage(result_image)).show()
+    return sitk.GetArrayFromImage(result_image)
 
 
 def main():
-    register_image_series_pet_to_ct()
+    register_image_series_pt2ct(ct_series_dir=r"F:\做好的分割数据\迟学梅\CT",
+                                pt_series_dir=r"F:\做好的分割数据\迟学梅\PT")
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    end_time = time.time()
+    print("time elapsed: {}".format(end_time - start_time))
