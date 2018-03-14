a321b97d50b0a5e39f7cccc7771928a697632823
shifvb <shifvb@gmail.com>
2018-03-14 18:19:28 +0800

    added mask display function

--- current_work/look_labels_app/gui/coronal_plane_gui.py
+++ current_work/look_labels_app/gui/coronal_plane_gui.py
@@ -6,13 +6,13 @@ from current_work.utils import Clock
 
 
 class CoronalPlaneGUI(tk.Toplevel):
-    def __init__(self, ct_arrs: np.ndarray, pt_arrs: np.ndarray, patient_info: dict):
+    def __init__(self, ct_arrs: np.ndarray, pt_arrs: np.ndarray, mask_arrs: np.ndarray, patient_info: dict):
         super().__init__()
         # 窗口设置
         self._window_size = (1000, 1024)  # height, width
         self.top_level = self
         self.top_level.title("CoronalPlaneView")
-        self.top_level.geometry("1024x1000+0+0")
+        self.top_level.geometry("1536x1000+0+0")
         self.top_level.bind("<Key-Left>", self.prev_page_callback)
         self.top_level.bind("<Key-Right>", self.next_page_callback)
         self.top_level.protocol("WM_DELETE_WINDOW", self.close_window_callback)
@@ -25,20 +25,26 @@ class CoronalPlaneGUI(tk.Toplevel):
         self.patient_info = patient_info
         self.ct_arrs = self.from_transverse_plane_to_coronal_plane(ct_arrs)
         self.pt_arrs = self.from_transverse_plane_to_coronal_plane(pt_arrs)
+        self.mask_arrs = self.from_transverse_plane_to_coronal_plane(mask_arrs[:, :, :, 0])
 
         # UI设置
+        print()
 
         # ct frame
         ct_frame = tk.Frame(self.top_level)
         ct_frame.grid(row=0, column=0)
         self.ct_canvas = tk.Canvas(ct_frame, width=512, height=1000)
         self.ct_canvas.pack()
-
         # pt frame
         pt_frame = tk.Frame(self.top_level)
         pt_frame.grid(row=0, column=1)
         self.pt_canvas = tk.Canvas(pt_frame, width=512, height=1000)
         self.pt_canvas.pack()
+        # mask frame
+        mask_frame = tk.Frame(self.top_level)
+        mask_frame.grid(row=0, column=2)
+        self.mask_canvas = tk.Canvas(mask_frame, width=512, height=1000)
+        self.mask_canvas.pack()
 
         # right most frame
         right_frame = tk.Frame(self.top_level)
@@ -49,29 +55,33 @@ class CoronalPlaneGUI(tk.Toplevel):
 
     def load_images(self):
         """在界面上加载图像"""
-        # 加载ct
+        # load ct image
         ct_arr = norm_image(self.ct_arrs[self.current_index])
         self.current_ct_img = ImageTk.PhotoImage(self.resize_to_fit_screen(ct_arr))
         self.ct_canvas.create_image(0, 0, image=self.current_ct_img, anchor=tk.NW)
-        # 加载pt
+        # load pt image
         pt_arr = norm_image(self.pt_arrs[self.current_index])
         self.current_pt_img = ImageTk.PhotoImage(self.resize_to_fit_screen(pt_arr))
         self.pt_canvas.create_image(0, 0, image=self.current_pt_img, anchor=tk.NW)
+        # load mask image
+        mask_arr = norm_image(self.mask_arrs[self.current_index])
+        self.current_mask_img = ImageTk.PhotoImage(self.resize_to_fit_screen(mask_arr))
+        self.mask_canvas.create_image(0, 0, image=self.current_mask_img, anchor=tk.NW)
 
-        # 设置title
+        # set title
         self.top_level.title("CoronalPlaneView ({} / {})".format(self.current_index + 1, self.total_img_num))
 
-    def from_transverse_plane_to_coronal_plane(self, ct_arrs):
+    def from_transverse_plane_to_coronal_plane(self, arrs):
         """从横断面转到冠状面进行数组转换，
         第一步是转秩变为冠状面，第二步是根据DICOM文件中实际像素间距，将图像拉伸"""
         # 转秩
-        ct_arrs = ct_arrs.transpose([1, 0, 2])
+        arrs = arrs.transpose([1, 0, 2])
         # 图像拉伸
         _ratio = self.patient_info['sliceThickness'] / self.patient_info['pixelSpacing'][1]
-        _old_size = ct_arrs[0].shape
+        _old_size = arrs[0].shape
         _new_size = [int(_) for _ in (_old_size[0] * _ratio, _old_size[1])]
         _new_size.reverse()  # PIL.Image.resize() receive format of (width, height), rather than (height, width)
-        return np.stack([np.array(Image.fromarray(_).resize(_new_size)) for _ in ct_arrs], axis=0)
+        return np.stack([np.array(Image.fromarray(_).resize(_new_size)) for _ in arrs], axis=0)
 
     def resize_to_fit_screen(self, arr: np.ndarray):
         """根据窗口大小缩放图像"""
